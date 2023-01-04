9ed9696621d964b679203b7bb574e2284a879007
webozza <webozza@gmail.com>
2023-01-05 01:00:27 +0800

    version update

--- classes/sets_custom_fields.php
+++ classes/sets_custom_fields.php
@@ -201,9 +201,11 @@ function set_presets_cb() {
             <script>
                 let selectedPresetRecords = <?= wp_json_encode($getpresetvalues[0]) ?>;
                 console.log('wp-encoded', selectedPresetRecords);
-                selectedPresetRecords.map((entries) => {
-                    jQuery(`#${entries.cat}`).val(entries.ids).trigger("change");
-                });
+                if(selectedPresetRecords !== null) {
+                    selectedPresetRecords.map((entries) => {
+                        jQuery(`#${entries.cat}`).val(entries.ids).trigger("change");
+                    });
+                }
             </script>
             <input type="hidden" name="save_preset_cards" value="">
         </div>
