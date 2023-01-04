a171d8e00486c5d251c7dbfa0b2bf03c4e8f2acb
webozza <webozza@gmail.com>
2023-01-05 03:22:30 +0800

    version updated

--- classes/sets_custom_fields.php
+++ classes/sets_custom_fields.php
@@ -198,18 +198,16 @@ function set_presets_cb() {
                 wp_reset_postdata();
             }
             ?>
-            <script>
-                jQuery(document).ready(function($) {
-                    let selectedPresetRecords = <?= wp_json_encode($getpresetvalues[0]) ?>;
-                    console.log('wp-encoded', selectedPresetRecords);
-                    if(selectedPresetRecords !== null) {
-                        selectedPresetRecords.map(entries => {
-                            $(`#${entries.cat}`).val(entries.ids).trigger("change");
-                        });
-                    }
-                });
-            </script>
             <input type="hidden" name="save_preset_cards" value="">
+            <script>
+                let selectedPresetRecords = <?= wp_json_encode($getpresetvalues[0]) ?>;
+                console.log('wp-encoded', selectedPresetRecords);
+                if(selectedPresetRecords !== null) {
+                    selectedPresetRecords.forEach(entries => {
+                        jQuery(`#${entries.cat}`).val(entries.ids).trigger("change");
+                    });
+                }
+            </script>
         </div>
     <?php
 }
