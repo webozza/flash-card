763f9a401758022dce4f0a81e9cfbc19534cbcd1
webozza <webozza@gmail.com>
2023-03-01 18:20:19 +0800

    version update

--- admin/partials/settings.php
+++ admin/partials/settings.php
@@ -63,10 +63,9 @@
     $roles_obj = new WP_Roles();
     $roles_names_array = $roles_obj->get_names();
     $getoptions = get_option('fc_rlink');
-    var_dump($getoptions);
     $redirectid = $getoptions['duplicate_redirect_id'];
     $redirectlink = $getoptions['duplicate_redirect_link'];
-    echo strlen($get_options['roles_selected']);
+    var_dump($get_options['roles_selected']);
 ?>
 
 <div class="wrap">
