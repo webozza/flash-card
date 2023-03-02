28e227005072fe17f6da5f06c26bea7734027dbd
webozza <webozza@gmail.com>
2023-03-02 14:05:55 +0800

    version update

--- includes/backend.php
+++ includes/backend.php
@@ -122,7 +122,13 @@
             $rolesselectedstripped = stripslashes($rolesselected);
             $rolesselectedarray = json_decode($rolesselectedstripped);
             foreach($rolesselectedarray as $selection) {
-                var_dump($selection);
+                $args = array(
+                    'role'    => $selection->roles,
+                    'orderby' => 'user_nicename',
+                    'order'   => 'ASC'
+                );
+                $users = get_users( $args );
+                var_dump($users->id);
             }
         }
     }
