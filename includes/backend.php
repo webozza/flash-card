6bbd366930ee9091dbd52a0a1eb420bd824fea2d
webozza <webozza@gmail.com>
2023-03-02 10:42:02 +0800

    version update

--- includes/backend.php
+++ includes/backend.php
@@ -110,7 +110,6 @@
     // Retrieve settings and process query on server
     function update_settings() {
         if(isset($_POST['backend_settings']) == '1') {
-            var_dump($_POST['selection_limits']);
             update_option('fc_rlink', array(
                 'duplicate_redirect_link' => $_POST['redirect_link'],
                 'duplicate_redirect_id' => $_POST['redirect_id'],
