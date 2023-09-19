db39f9b36b700a60ab066e0f852e3b6f4570f26b
talha-webozza <talha@webozza.com>
2023-09-19 12:12:57 +0600

    version update

--- flash-card.php
+++ flash-card.php
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'FLASH_CARD_VERSION', '29.6.56' );
+define( 'FLASH_CARD_VERSION', '29.6.57' );
 
 /**
  * The code that runs during plugin activation.
